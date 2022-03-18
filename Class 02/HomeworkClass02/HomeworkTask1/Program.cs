df8c3ab0eaf7f067deb58ce944b0085204a23f9d
Mario <96387178+miloshevskimario@users.noreply.github.com>
2022-03-18 18:34:17 +0100

    Add files via upload

---
+++ Class 02/HomeworkClass02/HomeworkTask1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace HomeworkTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create new console application “RealCalculator” that takes two numbers from the input and asks what operation would the user want to be done
+            //( +, - , * , / ). Then it returns the result.
+            //Test Data:
+            //Enter the First number: 10
+            //Enter the Second number: 15
+            //Enter the Operation: +
+            //Expected Output: The result is: 25
+
+            Console.WriteLine("Task 1: Real Calculator");
+
+            int num1;
+            int num2;
+            int result;
+
+            Console.WriteLine("Enter the first number");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter an operator");
+            Console.WriteLine(" + (PLUS)");
+            Console.WriteLine(" - (MINUS)");
+            Console.WriteLine(" * (MULTIPLY)");
+            Console.WriteLine(" / (DIVIDE)");
+
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    result = num1 + num2;
+                    Console.WriteLine($"Your result: {num1} + {num2} = " + result);
+                    break;
+
+                case "-":
+                    result = num1 - num2;
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + result);
+                    break;
+
+                case "*":
+                    result = num1 * num2;
+                    Console.WriteLine($"Your result: {num1} * {num2} = " + result);
+                    break;
+
+                case "/":
+                    result = num1 / num2;
+                    Console.WriteLine($"Your result: {num1} / {num2} =" + result);
+                    break;
+
+                default:
+                    Console.WriteLine("That was not a valid option");
+                    break;
+            }
+        }
+    }
+}
---
+++ Class 02/HomeworkClass02/HomeworkTask2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HomeworkTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create new console application “AverageNumber” that takes four numbers as input to calculate and print the average.
+            //Test Data:
+            //Enter the First number: 10
+            //Enter the Second number: 15
+            //Enter the third number: 20
+            //Enter the four number: 30
+            //Expected Output:
+            //The average of 10, 15, 20 and 30 is: 18
+
+            Console.WriteLine("Task 2: Average Number");
+      
+            int num1;
+            int num2;
+            int num3;
+            int num4;
+            
+            Console.WriteLine("Enter the first number: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number: ");
+            num3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the fourth number: ");
+            num4 = int.Parse(Console.ReadLine());
+
+            int average = (num1 + num2 + num3 + num4) / 4;
+
+
+            Console.WriteLine("The average of your numbers is : " + average);
+            Console.ReadLine();
+        }
+    }
+}
