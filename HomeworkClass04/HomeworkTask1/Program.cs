4aa0d260bbb586b4d42b4d409313bf52c4db5b33
Mario <96387178+miloshevskimario@users.noreply.github.com>
2022-04-04 01:14:43 +0200

    Add files via upload

---
+++ HomeworkClass04/HomeworkTask1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HomeworkTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a text");
+            string myString = Console.ReadLine();
+
+
+            string subString = myString.Substring(myString.Length - 5);
+            Console.WriteLine(subString);
+        }
+    }
+}
