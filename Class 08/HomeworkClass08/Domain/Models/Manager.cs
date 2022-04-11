c9a08c07b0a5ed81be3efd525eb4207c1c6f5582
Mario <96387178+miloshevskimario@users.noreply.github.com>
2022-04-11 03:02:11 +0200

    Add files via upload

---
+++ Class 08/HomeworkClass08/Domain/Models/Manager.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Enums;
+
+namespace Domain.Models
+{
+    public class Manager : Employee
+    {
+        private double _bonus { get; set; }
+
+        public Manager()
+        {
+        }
+
+        public Manager(string firstName, string lastName, double salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+            Role = RoleEnum.Manager;
+        }
+
+        public void AddBonus(double bonus)
+        {
+            _bonus += bonus;
+        }
+
+        public override double GetSalary()
+        {
+            return Salary + _bonus;
+        }
+    }
+}
---
+++ Class 08/HomeworkClass08/Exercise/Exercise.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Domain.Models;
+using Domain.Enums;
+
+namespace Domain
+{
+    public static class Exercise
+    {
+        private static void Main(string[] args)
+        {
+            Contractor contractor01 = new Contractor
+            {
+                FirstName = "Bob",
+                LastName = "Bobby",
+            };
+
+            Contractor contractor02 = new Contractor
+            {
+                FirstName = "Rick",
+                LastName = "Ricky", 
+            };
+
+            Manager manager01 = new Manager
+            {
+                FirstName = "Rob",
+                LastName = "Robby"
+            };
+
+            Manager manager02 = new Manager
+            {
+                FirstName = "John",
+                LastName = "Johny"
+            };
+
+            SalesPerson salesMan = new SalesPerson
+            {
+                FirstName = "Jack",
+                LastName = "Jacky"
+            };
+
+            object[] company = new object[5] { contractor01, contractor02, manager01, manager02, salesMan };
+
+            CEO ceo = new CEO("Ron", "Ronny", 1200, 20, new string[] { "Bob Bobby", "Rick Ricky", "Rob Robby", "John Johny", "Jack Jacky" });
+
+            Console.WriteLine("CEO:");
+
+            ceo.AddSharesPrice(200);
+            ceo.PrintInfo();
+            ceo.GetSalary();
+            ceo.PrintEmployees();
+
+        }
+    }
+}
