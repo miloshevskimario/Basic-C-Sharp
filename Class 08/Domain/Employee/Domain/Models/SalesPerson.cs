435f4d40078ce5c763655a9d15d25e5eab0e743a
Mario <96387178+miloshevskimario@users.noreply.github.com>
2022-04-11 02:41:24 +0200

    Add files via upload

---
+++ Class 08/Domain/Employee/Domain/Models/SalesPerson.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Enums;
+
+namespace Domain.Models
+{
+    public class SalesPerson : Employee
+    {
+        private double _successSaleRevenue { get; set; }
+
+        public SalesPerson()
+        {
+        }
+
+        public SalesPerson(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+
+            Salary = 500;
+            Role = RoleEnum.Sales;
+        }
+
+        public void AddSuccessRevenue(double revenue)
+        {
+            _successSaleRevenue += revenue;
+        }
+
+        public override double GetSalary()
+        {
+            if (_successSaleRevenue <= 2000)
+            {
+                return Salary + 500;
+            }
+
+            if (_successSaleRevenue > 2000 && _successSaleRevenue <= 5000)
+            {
+                return Salary + 1000;
+            }
+
+            return Salary + 1500;
+        }
+    }
+}
