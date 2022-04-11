66a57a542cfc409966c249fa0f2352c3381c0e4a
Mario <96387178+miloshevskimario@users.noreply.github.com>
2022-04-11 02:54:48 +0200

    Add files via upload

---
+++ Class 09/HomeworkClass09/HomeworkTask2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HomeworkTask2
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<int> integersList = new List<int>();
+
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                integersList.Add(random.Next(1, 100));
+            }
+
+            foreach (var integers in integersList)
+            {
+                Console.WriteLine(integers);
+            }
+
+            List<int> squaredNumbers = (List<int>)integersList.Select(x => x * x).ToList();
+
+            Console.WriteLine("Squared numbers");
+
+            foreach (var item in squaredNumbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Class 09/HomeworkClass09/HomeworkTask3/Models/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Task03.Enum;
+
+namespace Task03.Model
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+
+        public string Color { get; set; }
+
+        public int Age { get; set; }
+
+        public GenderEnum Gender { get; set; }
+    }
+}
