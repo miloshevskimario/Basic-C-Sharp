d7a72a96799cfa20a10e3ab87e8af7f0a1af79b6
Mario <96387178+miloshevskimario@users.noreply.github.com>
2022-04-11 02:09:24 +0200

    Add files via upload

---
+++ Class 05/HomeworkClass05/HomeworkTask2/Program.cs
@@ -0,0 +1,22 @@
+﻿using HomeworkTask2.Methods;
+using System;
+
+namespace HomeworkTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human(ReadInput("First name"), ReadInput("Last name"), ReadInput("Age"));
+
+            Console.WriteLine(human.GetPersonStats());
+
+            string ReadInput(string input)
+            {
+                Console.WriteLine("Enter " + input);
+                return Console.ReadLine();
+
+            }
+        }
+    }
+}
