b85de2f16aa44a8be59a01056767739e39e7b88d
Mario <96387178+miloshevskimario@users.noreply.github.com>
2022-04-11 02:52:58 +0200

    Add files via upload

---
+++ Class 08/HomeworkClass08/Domain/Models/Contractor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class Contractor : Employee
+    {
+        public double WorkHours { get; set; }
+
+        public int PayPerHour { get; set; }
+
+        public Manager Responsible { get; set; }
+        public string Name { get; set; }
+
+        public override double GetSalary()
+        {
+            Salary = WorkHours * PayPerHour;
+            return Salary;
+        }
+
+        public Manager CurrentPosition()
+        {
+            return Responsible;
+        }
+    }
+}
---
+++ Class 08/HomeworkClass08/Exercise/Exercise.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Domain.Models;
+using Domain.Enums;
+
+namespace Domain
+{
+    public static class Exercise
+    {
+        private static void Main(string[] args)
+        {
+            Contractor contractor01 = new Contractor
+            {
+                FirstName = "Bob",
+                LastName = "Bobby",
+
+            };
+
+            Contractor contractor02 = new Contractor
+            {
+                FirstName = "Rick",
+                LastName = "Ricky",
+                WorkHours = 80,
+                PayPerHour = 22
+            };
+
+            Manager manager01 = new Manager
+            {
+                FirstName = "Rob",
+                LastName = "Robby"
+            };
+
+            Manager manager02 = new Manager
+            {
+                FirstName = "John",
+                LastName = "Johny"
+            };
+
+            SalesPerson sale = new SalesPerson
+            {
+                FirstName = "Jack",
+                LastName = "Jacky"
+            };
+
+            object[] company = new object[5] { contractor01, contractor02, manager01, manager02, sale };
+
+            CEO ceo = new CEO("Ron", "Ronsky", 800, 30, new string[] { "Bob Bobby", "Rick Ricky", "Rob Robby", "John Johny", "Jack Jacky" });
+
+            Console.WriteLine("CEO:");
+
+            ceo.AddSharesPrice(350);
+            ceo.PrintInfo();
+            ceo.GetSalary();
+            ceo.PrintEmployees();
+
+            Console.ReadLine();
+        }
+    }
+}
